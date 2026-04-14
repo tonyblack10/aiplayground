a2a61d80955040138e384fc867314b3f8acfb775
Tony Costa <tonyblack10@gmail.com>
2026-04-13 23:39:25 -0300

    refactor login handling to use model attributes for error and success messages

--- src/main/java/io/tonyblack10/aiplayground/login/web/LoginController.java
+++ src/main/java/io/tonyblack10/aiplayground/login/web/LoginController.java
@@ -1,13 +1,19 @@
 package io.tonyblack10.aiplayground.login.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login() {
-        return "login";
+    public Mono<String> login(ServerWebExchange exchange, Model model) {
+        var params = exchange.getRequest().getQueryParams();
+        model.addAttribute("loginError", params.containsKey("error"));
+        model.addAttribute("logoutSuccess", params.containsKey("logout"));
+        return Mono.just("login");
     }
 }
